98a1bbf0e834a743acd6d5f1576b71254832da93
Stepan Derbenev <gespover863@gmail.com>
2020-01-06 04:46:26 +0400

    Added http/https processing and implemented the recording of full links in the DB

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask import request, json, redirect, url_for
+from flask import request, json, redirect
 import redis
 import re
 
 app = Flask(__name__)
-r = redis.Redis()
+_redis = redis.Redis()
 
 
 @app.route('/url', methods=['GET', 'POST'])
@@ -13,19 +13,19 @@ def url_to_hash():
     if not re.search('\w+\.\w+', url):
         return json.jsonify({'Была введена некорректная ссылка': url})
     else:
-        if r.exists(url) == 0:
+        if not _redis.exists(url):
+            if not re.findall(r'https?://\w+\.\w+', url):
+                url = 'https://' + url
             code = hash(url)
-            r.set(url, code)
-            r.set(code, url)
-        return json.jsonify({url: r.get(url).decode('utf-8')})
+            _redis.set(url, code)
+            _redis.set(code, url)
+        return json.jsonify({url: _redis.get(url).decode('utf-8')})
 
 
 @app.route('/code', methods=['GET', 'POST'])
 def to_website():
-    if r.exists(request.args['code']):
-        url = r.get(request.args['code']).decode('utf-8')
-        if not re.findall(r'https://\w+\.\w+', url):
-            url = 'https://' + url
+    if _redis.exists(request.args['code']):
+        url = _redis.get(request.args['code']).decode('utf-8')
         return redirect(url, code=302)
     else:
         return json.jsonify({'Данного кода не существует в базе данных': request.args['code']})
